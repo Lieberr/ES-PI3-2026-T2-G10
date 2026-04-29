7a5e7a19c7c4c69d3c7fe73c0d9a5d84bc6450f8
Lieberr <gustavoliebfigueira@gmail.com>
2026-04-29 19:31:29 -0300

    corrigindo pasta mescla_invest

---
+++ mescla_invest/lib/pages/login_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+
+              // Ícone
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: const Icon(Icons.rocket, color: Colors.white, size: 32),
+              ),
+
+              const SizedBox(height: 24),
+
+              const Text(
+                "Bem-vindo ao",
+                style: TextStyle(fontSize: 18),
+              ),
+
+              const Text(
+                "MesclaInvest",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+              const Text(
+                "Faça login para continuar",
+                style: TextStyle(color: Colors.grey),
+              ),
+
+              const SizedBox(height: 32),
+
+              // Email
+              const Text("E-mail"),
+              const SizedBox(height: 8),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "seu@email.com",
+                  hintStyle: TextStyle(
+                    color: Colors.black.withOpacity(0.5)
+                  ),
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              // Senha
+              const Text("Senha"),
+              const SizedBox(height: 8),
+
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock_outline),
+                  suffixIcon: const Icon(Icons.visibility_outlined),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 12),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 6, 64, 255)
+                  ),
+                  onPressed: () {},
+                  child: const Text("Esqueci minha senha"),
+                ),
+              ),
+
+              const SizedBox(height: 12),
+
+              // Botão
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: const Color.fromRGBO(21, 93, 252, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text("Entrar"),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text.rich(
+                    TextSpan(
+                      text: "Não tem uma conta? ",
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(
+                          text: "Cadastre-se",
+                          style: TextStyle(
+                            color: Color.fromRGBO(21, 93, 252, 1), // 👈 só essa parte azul
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
