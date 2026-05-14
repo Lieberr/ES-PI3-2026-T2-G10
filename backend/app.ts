0241b47f4d334bce0467d4d1caeb352069b82a50
leddixd <leodionel06@gmail.com>
2026-05-14 02:18:10 -0300

    Modelagem do Node.js

---
+++ backend/app.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+import { authRoutes } from './src/routes/authRoutes'
+import { ok } from 'node:assert'
+import { Timestamp } from 'firebase-admin/firestore'
+import { error } from 'node:console'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/health', (_req, res) => {
+    res.json({status: ok, Timestamp: new Date().toISOString() })
+}) 
+
+app.use('/auth', authRoutes)
+
+app.use((err: Error, _req: express.Request, res: express.Response, _next: express.NextFunction) =>{
+    console.error('erro não tratado:', err.message);
+    res.status(500).json({ error: 'Erro interno do servidor'
+    })
+    
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('Servidor rodando na porta ${PORT}')
+})
