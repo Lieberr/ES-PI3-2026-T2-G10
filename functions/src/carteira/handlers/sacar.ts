89236681257980fab77374197a97a6b441367361
leddixd <leodionel06@gmail.com>
2026-05-21 17:04:05 -0300

    criação do saque e do deposito no backend

---
+++ functions/src/carteira/handlers/sacar.ts
@@ -0,0 +1,40 @@
+import {onCall, CallableRequest, HttpsError} from "firebase-functions/v2/https";
+import { AtualizarSaldoInput } from "../types/carteira";
+import { atualizarSaldo, buscarCarteira, registrarOperacao } from "../repositories/carteiraRepository";
+import { Timestamp } from "firebase-admin/firestore";
+
+export const sacar = onCall(
+    async (request: CallableRequest<AtualizarSaldoInput>) => {
+        const data = request.data
+        if(data.valor < 0) {
+            throw new HttpsError(
+                "invalid-argument",
+                'O valor inserido é invalido.'
+            )
+        }
+        const {valor} = data;
+        const uid = request.auth?.uid;
+        if(!uid) {
+            throw new HttpsError("unauthenticated", 'Usuário não autenticado.')
+        }
+        const carteira = await buscarCarteira(uid)
+        if(!carteira){
+            throw new HttpsError("not-found", 'Carteira não encontrada')
+        }
+
+        if(valor > carteira.saldo){
+            throw new HttpsError("invalid-argument", 'Saldo insuficiente para realizar o saque')
+        }
+
+        const novoSaldo = carteira.saldo - valor;
+        await atualizarSaldo(uid, novoSaldo);
+
+        await registrarOperacao({
+            uid,
+            tipo: "saque",
+            valor,
+            realizadoEm: Timestamp.now(),
+        })
+        return {mensagem: "Saque realizado com sucesso."}
+    }
+)
--- functions/src/carteira/repositories/carteiraRepository.ts
+++ functions/src/carteira/repositories/carteiraRepository.ts
@@ -1,6 +1,6 @@
 // Feito por Leonardo Dionel RA: 25010092
 
-import {Carteira} from "../types/carteira";
+import {Carteira, Operacao} from "../types/carteira";
 import {db} from "../../shared/firebase";
 import {Timestamp} from "firebase-admin/firestore";
 
@@ -17,4 +17,40 @@ export async function criarCarteira(uid: string): Promise<void> {
   };
   const carteirasCollection = db.collection("carteiras");
   await carteirasCollection.doc(uid).set(carteira);
+}
+
+/**
+ * Atualiza o saldo do usuario
+ * @param {Operacao} operacao - UID do usuário no Firebase Auth.
+ * @return {Promise<void>}
+ */
+export async function registrarOperacao(operacao: Operacao): Promise<void> {
+  await db.collection("carteiras").doc(operacao.uid)
+    .collection("operacoes").doc()
+    .set(operacao);
+}
+
+/**
+ * Atualiza o saldo do usuario
+ * @param {string} uid - UID do usuário no Firebase Auth.
+ * @param {number} novoSaldo - Novo saldo do usuario
+ * @return {Promise<void>}
+ */
+export async function atualizarSaldo(
+  uid: string, novoSaldo: number
+): Promise<void> {
+  await db.collection("carteiras").doc(uid).update({saldo: novoSaldo});
+}
+
+
+/**
+ * busca a carteira do usuario
+ * @param {string} uid - UID do usuário no Firebase Auth.
+ * @return {Promise<Carteira | null>}
+ */
+export async function buscarCarteira(uid: string): Promise<Carteira | null> {
+  const carteirasCollection = db.collection("carteiras");
+  const doc = await carteirasCollection.doc(uid).get();
+  if (!doc.exists) return null;
+  return doc.data() as Carteira;
 }
