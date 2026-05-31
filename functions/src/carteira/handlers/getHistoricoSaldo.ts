00048eac214955f4a5c5b2dda60bd00c1df42b94
Lieberr <gustavoliebfigueira@gmail.com>
2026-05-31 02:03:53 -0300

    Backend criado para pegar historico do saldo

--- functions/src/carteira/handlers/getHistoricoSaldo.ts
+++ functions/src/carteira/handlers/getHistoricoSaldo.ts
@@ -1,8 +1,7 @@
 // Feito por Gustavo lieb Ra: 24023376
 
-import { CallableContext, HttpsError, onCall } from "firebase-functions/v1/https";
-import { db } from "../../shared/firebase";
-import { CallableRequest } from "firebase-functions/https";
+import {CallableRequest, HttpsError, onCall} from "firebase-functions/v2/https";
+import {db} from "../../shared/firebase";
 
 export const getHistoricoSaldo = onCall(
     async (request: CallableRequest<Record<string, never>>) => {
@@ -15,29 +14,42 @@ export const getHistoricoSaldo = onCall(
             )
         }
 
-        const snap = await db
+        const snapMovimentacoes = await db
+                .collection("carteiras")
+                .doc(uid)
+                .collection("movimentacoes")
+                .orderBy("realizadoEm", "asc")
+                .get();
+        
+                
+        const snapOperacoes = await db
                 .collection("carteiras")
                 .doc(uid)
                 .collection("operacoes")
-                .orderBy("realizadosEm", "asc")
+                .orderBy("realizadoEm", 'asc')
                 .get();
         
-        if (snap.empty) {
+        const todasOps = [
+            ...snapMovimentacoes.docs.map((doc) => doc.data()),
+            ...snapOperacoes.docs.map((doc) => doc.data()),
+            ].sort((a, b) => a.realizadoEm.toMillis() - b.realizadoEm.toMillis());
+        
+        if (todasOps.length === 0) {
             return {pontos: []};
         }
 
         let saldoAcumulado = 0
         const pontos: {label: string; saldo: number}[] = [];
 
-        snap.docs.forEach((doc) => {
-            const op = doc.data();
+        todasOps.forEach((op) => {
+            
             if(op.tipo === "deposito") {
                 saldoAcumulado += op.valor;
             } else if(op.tipo === "saque") {
                 saldoAcumulado -= op.valor;
             }
 
-            const data = op.realizadosEm.toDate();
+            const data = op.realizadoEm.toDate();
             const label = `${data.getDate()}/${data.getMonth() + 1}`;
 
             pontos.push({label, saldo: saldoAcumulado});
