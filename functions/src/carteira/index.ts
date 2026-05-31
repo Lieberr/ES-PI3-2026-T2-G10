60a45ba43c77b70ae2e34a502255c0791796c8f0
Lieberr <gustavoliebfigueira@gmail.com>
2026-05-31 15:57:45 -0300

    Backend modificado para portfolio

--- functions/src/carteira/index.ts
+++ functions/src/carteira/index.ts
@@ -7,3 +7,4 @@ export {venderTokenPrimario} from "./handlers/venderTokenPrimario";
 export {getPortfolio} from "./handlers/getPortfolio";
 export {getCarteira} from "./handlers/getCarteira";
 export {getHistoricoSaldo} from "./handlers/getHistoricoSaldo";
+export {getHistoricoPortfolio} from "./handlers/getHistoricoPortfolio";
